575a0712a2af8dd7e4c78c9a9774383def1efb1a
steev42 <steev@2d12.com>
2023-09-02 17:29:32 -0400

    Updates to test git

--- src/main/java/io/bellwood/springbootinterview/Application.java
+++ src/main/java/io/bellwood/springbootinterview/Application.java
@@ -8,6 +8,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+        int a;
+        a = 3;
+        a = a + 2;
+        System.out.print(a);
+        }
 
 }
